45cd9a895be098fba17eb4ad2062c4b5c7d1c938
Vietdz123 <tienviet153153@gmail.com>
2024-01-07 16:10:15 +0700

    add concurrency readme

---
+++ AsyncAwaitLearning/AsyncAwaitLearning/AsyncAwaitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitLearningApp.swift
+//  AsyncAwaitLearning
+//
+//  Created by Three Bros on 06/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
